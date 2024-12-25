aac6510a8495b5d29a8cc7097286cebaceff4860
Ömer Apaydın <omerapaydin90@gmail.com>
2024-12-25 19:30:45 +0300

    Initial Commit

---
+++ CyrptoSUI/CyrptoSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyrptoSUIApp.swift
+//  CyrptoSUI
+//
+//  Created by Ömer on 25.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CyrptoSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CyrptoSUITests/CyrptoSUITests.swift
@@ -0,0 +1,17 @@
+//
+//  CyrptoSUITests.swift
+//  CyrptoSUITests
+//
+//  Created by Ömer on 25.12.2024.
+//
+
+import Testing
+@testable import CyrptoSUI
+
+struct CyrptoSUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
